1a1e3a22e3e24322da89e96df9eef66b007cac88
nishan akalanka <hdnakalanka@gmail.com>
2023-06-28 17:51:04 +0530

    delete book completed backend-nodejs

--- back-end-node-js/src/main.ts
+++ back-end-node-js/src/main.ts
@@ -1,1 +1,10 @@
-console.log("working");
+import express,{json} from 'express';
+import {router} from "./api/book"
+import cors from 'cors';
+
+const app=express();
+app.use(cors());
+app.use(json());
+app.use("/api/v1/books",router);
+app.listen(8081,()=>console.log("Server has been started at 8081"))
+
