bffda7d9f04f62f726600d92adb63d251bd6e0d2
Sajith Abeygunawardana <abeygunawardana1995@gmail.com>
2023-06-28 18:01:47 +0530

    save and update complete

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Book} from "./dto/book";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  bookList:Array<Book>=[];
+    constructor(private http:HttpClient) {
+      this.getBooks(http);
+}
 
+saveBook(txtIsbn:HTMLInputElement,txtTitle:HTMLInputElement):void{
+  if(!(txtIsbn.value.trim()&& txtTitle.value.trim())){
+    txtIsbn.select();
+    txtTitle.select();
+    return;
+    }
+  this.http.post<Book>('http://localhost:8080/app/api/v1/books',
+    new Book(txtIsbn.value,txtTitle.value)).subscribe(book =>{
+      this.bookList.push(book);
+      txtIsbn.value='';
+      txtIsbn.focus();
+    });
+  }
+  updateBook(txtIsbn:HTMLInputElement,txtTitle:HTMLInputElement):void{
+    if(!(txtIsbn.value.trim()&& txtTitle.value.trim())){
+      txtIsbn.select();
+      txtTitle.select();
+      return;
+    }
+    this.http.patch<Book>('http://localhost:8080/app/api/v1/books',
+      new Book(txtIsbn.value,txtTitle.value)).subscribe(book =>{
+      this.bookList.push(book);
+      txtIsbn.value='';
+      txtIsbn.focus();
+    });
+    this.getBooks(this.http);
+  }
+  getBooks(http:HttpClient):void{
+    http.get<Array<Book>>('http://localhost:8080/app/api/v1/books')
+      .subscribe(bookList=>this.bookList=bookList);
+  }
 }
---
+++ front-end/src/app/dto/book.ts
@@ -0,0 +1,7 @@
+export class Book {
+  constructor(public ISBN: string,
+              public Title: string){
+
+  }
+
+}
